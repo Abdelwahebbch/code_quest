5b972c678ad37690625e576ed608357b45bb8095
Abdelwaheb <bouchahwaabdelwaheb@fsg.u-gabes.tn>
2026-02-15 13:47:46 +0100

    Add Google SignIn button

---
+++ lib/widgets/google_sign_in_button.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class GoogleSignInButton extends StatelessWidget {
+  const GoogleSignInButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: 45,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(12),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withValues(alpha: 0.05),
+            blurRadius: 10,
+            offset: const Offset(0, 4),
+          )
+        ],
+      ),
+      child: InkWell(
+        borderRadius: BorderRadius.circular(24),
+        onTap: () {},
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.network(
+              "https://developers.google.com/identity/images/g-logo.png",
+              height: 24,
+            ),
+            const SizedBox(width: 12),
+            const Text(
+              "Continue with Google",
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.w600,
+                color: Colors.black87,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
