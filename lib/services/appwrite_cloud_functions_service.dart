ee2076cfba8a69f842b9d7c455bed871d59446e9
Abdelwaheb <abdelwahebbouchahwa52@gmail.com>
2026-03-30 08:51:15 +0100

    - add createLearningPath FN

--- lib/services/appwrite_cloud_functions_service.dart
+++ lib/services/appwrite_cloud_functions_service.dart
@@ -4,12 +4,13 @@ import 'dart:convert';
 
 import 'package:pfe_test/models/message_model.dart';
 import 'package:pfe_test/models/party_model.dart';
+import 'package:pfe_test/models/resolve_user_profile.dart';
 import 'package:pfe_test/models/user_info_model.dart';
 
 import '../models/mission_model.dart';
 
 class AppwritecloudfunctionsService extends ChangeNotifier {
-  Future<Map<String, dynamic>> sendMessage(Message message) async {
+  static Future<Map<String, dynamic>> sendMessage(Message message) async {
     final res = await http.post(
         Uri.parse('https://698a84430024b427a4a4.fra.appwrite.run/'),
         body: message.finalMessage);
@@ -23,7 +24,7 @@ class AppwritecloudfunctionsService extends ChangeNotifier {
     }
   }
 
-  Future<void> createCustomMissions() async {
+  static Future<void> createCustomMissions() async {
     try {
       final res = await http
           .get(Uri.parse('https://6995ccc5002bc1b94906.fra.appwrite.run/'));
@@ -34,7 +35,34 @@ class AppwritecloudfunctionsService extends ChangeNotifier {
     }
   }
 
-  Future<List<dynamic>> checkAnwser(
+  static Future<void> createLearningPath(
+      ResolvedProfile profile, String userId) async {
+    try {
+      final res = await http.post(
+          Uri.parse('https://69c8037600042b81ce1b.fra.appwrite.run/'),
+          body: {
+            "userId": userId,
+            "profile": {
+              "Topic": profile.language,
+              "currentLevel": profile.currentLevel,
+              "milestoneCount": profile.milestoneCount,
+              "conceptsPerMilestone": profile.conceptsPerMilestone,
+              "focusArea": profile.focusArea,
+              "commitment": profile.commitment,
+            }
+          });
+      debugPrint(res.body);
+    } catch (e) {
+      debugPrint("Error when create learning path");
+      rethrow;
+    }
+  }
+
+  // static Future<void> loadLearningPath() {
+  //   try {} catch (e) {}
+  // }
+
+  static Future<List<dynamic>> checkAnwser(
       UserInfo user, Mission mission, String solution) async {
     try {
       final res = await http.post(
@@ -67,8 +95,7 @@ class AppwritecloudfunctionsService extends ChangeNotifier {
     }
   }
 
-  Future<void> requestForPartyQuizzes(
-      Party p, String difficulty) async {
+  static Future<void> requestForPartyQuizzes(Party p, String difficulty) async {
     try {
       final res = await http.post(
           Uri.parse('https://69b606670033bd00ed26.fra.appwrite.run/'),
@@ -78,7 +105,7 @@ class AppwritecloudfunctionsService extends ChangeNotifier {
             "nb_rounds": p.totalRounds
           }));
 
-     // List<dynamic> data = [];
+      // List<dynamic> data = [];
       final Map<String, dynamic> decoded = jsonDecode(res.body);
       //final String responseString = decoded["response"];
       //data = jsonDecode(responseString);
