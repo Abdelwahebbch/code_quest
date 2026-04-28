cf41b9b9321504fdfdaec48d94677c2188555803
mahdibenmakhlouf <ben.makhlouf.mahdi@gmail.com>
2026-04-28 23:52:43 +0100

    add the elo rank to the profile screen

--- lib/views/profile/profile_screen.dart
+++ lib/views/profile/profile_screen.dart
@@ -37,6 +37,13 @@ class _ProfileScreenState extends State<ProfileScreen> {
     final auth = Provider.of<AuthProvider>(context, listen: false);
     final user = authService.authProvider.currentUser;
     final String userImage = authService.progress.imageId;
+    List<String> images=["iron1","iron2","iron3","bronze1","bronze2","bronze3","silver1","silver2","silver3","gold1","gold2","gold3"];
+    List<String> elos=["Iron 1","Iron 2","Iron 3","Bronze 1","Bronze 2","Bronze 3","Silver 1","Silver 2","Silver 3","Gold 1","Gold 2","Gold 3"];
+    int elo=authService.progress.elo;
+    int index=0;
+    if(elo<2300) index=elo ~/ 100;
+    else index=22;
+    String image="assets/icon/${images[index]}.png";
     NetworkImage dataBaseImage = NetworkImage(
         'https://fra.cloud.appwrite.io/v1/storage/buckets/69891b1d0012c9a7e862/files/$userImage/view?project=697295e70021593c3438&mode=admin');
     if (!isReady) {
@@ -106,13 +113,30 @@ class _ProfileScreenState extends State<ProfileScreen> {
               const SizedBox(height: 32),
               _buildStatRow(context),
               const SizedBox(height: 32),
-              _buildSectionTitle(context, "Earned Badges"),
+              _buildSectionTitle(context, "Elo Rank"),
               const SizedBox(height: 16),
-              _buildBadgeGrid(context),
-              const SizedBox(height: 16),
-              _buildSectionTitle(context, "Learning Progress"),
-              const SizedBox(height: 16),
-              _buildProgressList(context),
+              Center(child: Image.asset(image,height: 100,)),
+              Center(child: Text(elos[index],style: const TextStyle(fontSize: 30,color: Colors.white),)),
+              const SizedBox(height: 20,),
+              ClipRRect(
+              borderRadius: BorderRadius.circular(10),
+              child: LinearProgressIndicator(
+                value: (elo%100)/100,
+                backgroundColor: Colors.white.withValues(alpha: 0.2),
+                valueColor: const AlwaysStoppedAnimation<Color>(Colors.white),
+                minHeight: 10,
+              ),
+            ),
+             Padding(
+               padding:  const EdgeInsets.only(left: 10.0,right: 10,top: 5),
+               child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Text("Elo Progress",style: TextStyle(color: Colors.white),),
+                  Text("${elo%100}/100",style: const TextStyle(color: Colors.white)),
+                ],
+               ),
+             )
             ],
           ),
         ),
@@ -152,194 +176,5 @@ class _ProfileScreenState extends State<ProfileScreen> {
     );
   }
 
-  Widget _buildBadgeGrid(context) {
-    final authService = Provider.of<DataProvider>(context, listen: false);
-    List<String> earnBadges = authService.progress.earnedBadges;
-    List<Map<String, dynamic>> allBadges = [
-      {
-        'name': 'Bug Hunter',
-        'desc': 'Fix 10 debugging missions',
-        'icon': Icons.bug_report,
-        'color': Colors.green,
-        'unlocked': false
-      },
-      {
-        'name': 'Code Ninja',
-        'desc': 'Complete 5 missions without hints',
-        'icon': Icons.bolt,
-        'color': Colors.orange,
-        'unlocked': false
-      },
-      {
-        'name': 'Test Master',
-        'desc': 'Write 20 unit tests',
-        'icon': Icons.verified,
-        'color': Colors.blue,
-        'unlocked': false
-      },
-      {
-        'name': 'Fast Learner',
-        'desc': 'Complete 3 missions in one day',
-        'icon': Icons.speed,
-        'color': Colors.purple,
-        'unlocked': false
-      },
-      {
-        'name': 'Architect',
-        'desc': 'Design a complex system',
-        'icon': Icons.architecture,
-        'color': Colors.red,
-        'unlocked': false
-      },
-      {
-        'name': 'Clean Coder',
-        'desc': 'Maintain high code quality',
-        'icon': Icons.cleaning_services,
-        'color': Colors.teal,
-        'unlocked': false
-      },
-      {
-        'name': 'Team Player',
-        'desc': 'Review 5 peer solutions',
-        'icon': Icons.groups,
-        'color': Colors.indigo,
-        'unlocked': false
-      },
-      {
-        'name': 'AI Whisperer',
-        'desc': 'Ask 50 insightful questions',
-        'icon': Icons.psychology,
-        'color': Colors.pink,
-        'unlocked': false
-      },
-    ];
-    List<Map<String, dynamic>> badges = [];
-    for (int i = 0; i < allBadges.length; i++) {
-      if (earnBadges.contains(allBadges[i]['name'])) {
-        badges.add(allBadges[i]);
-      }
-    }
-    return GridView.builder(
-      shrinkWrap: true,
-      physics: const NeverScrollableScrollPhysics(),
-      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 4,
-        mainAxisSpacing: 16,
-        crossAxisSpacing: 16,
-      ),
-      itemCount: badges.length,
-      itemBuilder: (context, index) {
-        return Column(
-          children: [
-            Container(
-              padding: const EdgeInsets.all(8),
-              decoration: BoxDecoration(
-                color: (badges[index]['color'] as Color).withValues(alpha: 0.1),
-                shape: BoxShape.circle,
-                border: Border.all(
-                    color: (badges[index]['color'] as Color)
-                        .withValues(alpha: 0.5)),
-              ),
-              child: Icon(badges[index]['icon'] as IconData,
-                  color: badges[index]['color'] as Color, size: 24),
-            ),
-            const SizedBox(height: 4),
-            Text(badges[index]['name'] as String,
-                style: const TextStyle(fontSize: 8),
-                textAlign: TextAlign.center),
-          ],
-        );
-      },
-    );
-  }
-
-  Widget _buildProgressList(context) {
-    final authService = Provider.of<DataProvider>(context, listen: false);
-    Map<String, dynamic> progress = authService.progress.badgesProgress;
-    int missionsCompletedToday = 0;
-    for (int i = 0; i < authService.progress.missions.length; i++) {
-      if (authService.progress.missions[i].isCompleted) {
-        missionsCompletedToday += 1;
-      }
-    }
-    double bugHunter =
-        (progress['debug'] / 10) >= 1 ? 1 : progress['debug'] / 10;
-    double codeNinja =
-        ((authService.progress.nbMissionCompletedWithoutHints / 10) * 2) >= 1
-            ? 1
-            : (authService.progress.nbMissionCompletedWithoutHints / 10) * 2;
-    double TestMaster =
-        ((progress['test'] / 10) * 2) >= 1 ? 1 : (progress['test'] / 10) * 2;
-    double FastLearner =
-        missionsCompletedToday / 3 >= 1 ? 1 : missionsCompletedToday / 3;
-    double Architect =
-        progress['ordering'] / 10 >= 1 ? 1 : progress['ordering'] / 10;
-    double CleanCoder =
-        ((progress['complete'] / 10 > 1 ? 1 : progress['complete'] / 10) +
-                (authService.progress.totalFailures / 30 > 1
-                    ? 1
-                    : authService.progress.totalFailures / 30)) /
-            2;
-    double TeamPlayer = ((progress['singleChoice'] / 10 > 1
-                ? 1
-                : progress['singleChoice'] / 10) +
-            (progress['multipleChoice'] / 10 > 1
-                ? 1
-                : progress['multipleChoice'] / 10)) /
-        2;
-    double AIWhisperer = (authService.progress.totalAIQuestions / 50) > 1
-        ? 1
-        : (authService.progress.totalAIQuestions / 50);
-
-    //TODO : lazem dynamique
-    return Column(
-      children: [
-        _buildProgressItem("Bug Hunter", bugHunter),
-        const SizedBox(height: 12),
-        _buildProgressItem("Code Ninja", codeNinja),
-        const SizedBox(height: 12),
-        _buildProgressItem("Test Master", TestMaster),
-        const SizedBox(height: 12),
-        _buildProgressItem("Fast Learner", FastLearner),
-        const SizedBox(height: 12),
-        _buildProgressItem("Architect", Architect),
-        const SizedBox(height: 12),
-        _buildProgressItem("Clean Coder", CleanCoder),
-        const SizedBox(height: 12),
-        _buildProgressItem("Team Player", TeamPlayer),
-        const SizedBox(height: 12),
-        _buildProgressItem("AI Whisperer", AIWhisperer),
-        const SizedBox(height: 12),
-      ],
-    );
-  }
-
-  Widget _buildProgressItem(String title, double progress) {
-    return Container(
-      padding: const EdgeInsets.all(16),
-      decoration: BoxDecoration(
-        color: AppTheme.cardColor,
-        borderRadius: BorderRadius.circular(12),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text(title, style: const TextStyle(fontWeight: FontWeight.bold)),
-              Text("${(progress * 100).toInt()}%"),
-            ],
-          ),
-          const SizedBox(height: 8),
-          LinearProgressIndicator(
-            value: progress,
-            backgroundColor: Colors.white10,
-            valueColor:
-                const AlwaysStoppedAnimation<Color>(AppTheme.primaryColor),
-          ),
-        ],
-      ),
-    );
-  }
+  
 }
